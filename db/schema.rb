f0db56f9b6e650d4a9347ab198bda5754aa0b63a
wafeBreaker <s0558645@htw-berlin.de>
2021-06-17 17:12:06 +0200

    Datenbank

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_06_17_111313) do
+
+  create_table "external_dbs", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
